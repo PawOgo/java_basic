f01ed792e88ac5a4b7162fc7b87ad09535bae332
PawOgo <pawelogorzeja@outlook.com>
2019-11-15 19:00:08 +0100

    3 dzień kursu

---
+++ src/variables/Operators.java
@@ -0,0 +1,46 @@
+package variables;
+
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 4;
+//        post inkrementacja
+        System.out.println(i++);
+//        po ywpisywaniu zmienna jest inkrementowana
+        System.out.println(i);
+
+//        preidekrementacja
+        System.out.println(--j);
+        System.out.println(j);
+
+        System.out.println((j++) * (--i + j--) + (++i));
+//        3*5+
+
+//        sprawdz czy uzytkownik ma 18 lat
+//        w jaki sposób ma sprawdzić
+//        jezeli jego wiek jest >= 18 lat to ywpisz jest ełnoletnie
+//        w przewciwnym razie wypisz nie jesteś pełnoeltni
+
+        int age = 44;
+//        utworzenie obiektu SCanner pozwalajcacego na wprowadzenie warosci do konsoli -> System.in
+        Scanner scanner = new Scanner(System.in);
+//        uzytkownik wprowdza do zmiennej age typu int metoda nextInt()
+        System.out.println("Wprowadź swój wiek: ");
+        age = scanner.nextInt();
+
+//        uzytkownik wprowadza wartosc zdo mziennej age
+
+        String isMature = age >= 18 ? "użytkownik jest pełnoletni: " : "nie jesteś pełnoletni";
+
+//        System.out.println("Twój wiek to: " + age + " - " + isMature); POWTÓRZYC!!!!!
+
+        System.out.printf("Twój wiek to: %d - %s\n",  age, isMature);
+//        czy wiek zawiera się od 0 - 120 jesli tak to jestes człowiekiem jesli nie orbotem
+
+        String isHuman  = age <= 120 ? "jesteś człowiekiem" : "jesteś robotem";
+        System.out.println(isHuman);
+
+    }
+}
--- src/variables/TypeString.java
+++ src/variables/TypeString.java
@@ -1,6 +1,7 @@
 package variables;
 
 public class TypeString {
+
     public static void main(String[] args) {
 
         String text = "Dowolny napis 1 1.124";
@@ -26,6 +27,43 @@ public class TypeString {
         String name2 = "ala";
         System.out.println(name1.equals(name2));
         System.out.println(name1.length() >= name2.length());
-        
+
+//        spoznienie 15 min co meli rdalej
+
+
+//        sprawdzanie czy napsi jest palindromem
+
+        String txt  = "KAJAK";
+        System.out.println(txt);
+//        utworzylismy obiekt klasy StingBuffer ->edytowalny String
+        StringBuffer editableTxt = new StringBuffer(txt);
+//        przypisanie odwrconego napisu do zmiennej String -> nieedytowalnej
+        String txtReserved = editableTxt.toString();
+        System.out.println(editableTxt);
+
+        System.out.println("Czy napis jest palindromem: " + txt.equals(txtReserved));
+
+//        inny sposb
+        String login = "MikiKru";
+        System.out.println(login.equals("mikikru"));
+        System.out.println(login.toUpperCase().equals("mikikru".toUpperCase()));
+
+        String sentence =  "byc albo nie byc oto jest pytanie";
+//        podziel zdaie na wyrazy
+//        separato - spacja
+        String [] words = sentence.split(" ");
+
+        System.out.println(words[0]);
+        System.out.println(words[1]);
+        System.out.println(words[2]);
+        System.out.println(words[3]);
+        System.out.println(words[4]);
+        System.out.println(words[5]);
+        System.out.println(words[6]);
+
+
+
+
+
     }
 }
